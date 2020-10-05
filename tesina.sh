7fec02e24aeaa6f0c7ecc719cae6160c98e7c987
Emanuel Balcazar <emanuelbalcazar13@gmail.com>
2020-10-05 16:04:10 -0300

    Se agrego un script sh para facilitar algunos comandos

---
+++ tesina.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Obtengo la ruta absoluta del proyecto.
+root="$(pwd)"
+
+# Evaluo el comando entrante
+case "$1" in
+
+    install)
+        echo "[install] instalando dependencias en crawl-extractors"
+        cd $root/crawl-extractors
+        npm install
+
+        cd $root/search-engine
+        echo "[install] instalando dependencias en search-engine"
+        npm install
+
+        cd $root/server
+        echo "[install] instalando dependencias en el servidor"
+        npm install 
+    ;;
+
+    migrate)
+        echo -e "Ejecutando migración en el servidor"
+        cd $root/server
+        adonis migration:run
+    ;;
+
+    seed)
+        echo -e "Ejecutando seeder en el servidor"
+        cd $root/server
+        adonis seed
+    ;;
+
+    *)
+        echo "Comando no reconocido"
+    ;;
+
+esac
