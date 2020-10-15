9daf09abd2a21a3e3e152eafbc738805512a9bb8
Emanuel Balcazar <emanuelbalcazar13@gmail.com>
2020-10-15 14:21:00 -0300

    Se agregan exports sql de backup

---
+++ archivos/sql/equations_202010121354.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.equations ("dateToFind",site_id,"start","siteSearchFilter","lastExecution",active) VALUES 
+('2020-03-14',4,61,'i','2020-10-12',true)
+,('2020-04-09',1,21,'i','2020-10-12',true)
+,('2020-04-01',3,31,'i','2020-10-12',true)
+,('2020-03-25',5,41,'i','2020-10-12',true)
+,('2020-03-18',2,91,'i','2020-10-12',true)
+;
